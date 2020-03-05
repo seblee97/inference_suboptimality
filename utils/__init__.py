285e232cae6aa5801a35bf9b133a88838404bbc9
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-03-05 23:02:27 +0000

    update to pacakge init

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
 from .parameters import InferenceGapParameters
 from .dataloaders import mnist_dataloader
 
-from .math_operations import log_normal, log_bernoulli
+from .math_operations import log_normal, binary_loss_array
