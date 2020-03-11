38b9a0743e5a4e3b8a07602e1d7646b9b450730a
Mandrenkov <M.Andrenkov@gmail.com>
2020-03-11 21:43:22 +0000

    Changed the binary cross-entropy reduction mode from 'sum' to 'mean' to avoid overwhelming the KL divergence.

--- models/loss_modules/gaussian_loss.py
+++ models/loss_modules/gaussian_loss.py
@@ -11,26 +11,16 @@ class gaussianLoss(baseLoss):
         baseLoss.__init__(self)
 
     def compute_loss(self, x, vae_output):
-        
         vae_reconstruction = vae_output['x_hat']
+        mean, log_var = vae_output['params']
 
-        params = vae_output['params']
-        mu = params[0]
-        var = params[1]
-
-        reconstruction_loss = F.binary_cross_entropy(vae_reconstruction, x, reduction='sum')
-
-        kl_loss = 0.5 * torch.sum(torch.exp(var) + mu**2 - 1.0 - var)
+        # Assuming a normal Gaussian prior and a fully-factorized Gaussian approximation,
+        # the loss function is detailed in https://arxiv.org/pdf/1907.08956.pdf.
+        reconstruction_loss = F.binary_cross_entropy(vae_reconstruction, x, reduction='mean')
+        kl_loss = torch.sum(torch.exp(log_var) + mean**2 - 1 - log_var) / 2
 
         return reconstruction_loss + kl_loss
 
         # loss, rec, kl, bpd = calculate_loss(x_mean, data, z_mu, z_var, z0, zk, ldj, args, beta=beta)
 
         # loss, rec, kl = binary_loss_function(x_mean, x, z_mu, z_var, z_0, z_k, ldj, beta=beta)
-
-        
-        
-        # elbo = reconstruction_loss + logpz - plogqz
-        # loss = -elbo
-
-        # raise NotImplementedError
