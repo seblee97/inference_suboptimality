f7683d13ac0de975cb5179f751b82a5a6b11cd68
Maxence Draguet <maxdraguet@hotmail.com>
2020-03-14 19:40:41 +0000

    dataloader for the binarised MNIST

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
 from .parameters import InferenceGapParameters
 from .dataloaders import mnist_dataloader
-
+from .dataloaders import binarised_mnist_dataloader
 from .math_operations import log_normal, binary_loss_array
--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -2,10 +2,11 @@ import torch
 import torchvision
 
 from .custom_torch_transforms import TensorFlatten
-
+import os
+import numpy as np
 def mnist_dataloader(data_path: str, batch_size: int, train:bool=True):
     """
-    Load mnist image data from specified, convert to grayscaled tensors, flatten, return dataloader
+    Load mnist image data from specified, convert to grayscaled (then binarised) tensors, flatten, return dataloader
 
     :param data_path: full path to data directory
     :param batch_size: batch size for dataloader
@@ -14,12 +15,91 @@ def mnist_dataloader(data_path: str, batch_size: int, train:bool=True):
     """
     # transforms to add to data
     transform = torchvision.transforms.Compose([
-        torchvision.transforms.Grayscale(),
-        torchvision.transforms.ToTensor(),
-        TensorFlatten()
-    ])
+                                                torchvision.transforms.Grayscale(),
+                                                torchvision.transforms.ToTensor(),
+                                                lambda x: x>=0.5,
+                                                lambda x: x.float(),
+                                                TensorFlatten()
+                                                ])
 
     mnist_data = torchvision.datasets.MNIST(data_path, transform=transform, train=train)
     dataloader = torch.utils.data.DataLoader(mnist_data, batch_size=batch_size, shuffle=True)
 
+    return dataloader
+
+def binarised_mnist_dataloader(data_path: str, batch_size: int, train:bool=True):
+    """
+        Load binarised mnist image data from specified, convert to  tensors, flatten, return dataloader
+        
+        :param data_path: full path to data directory
+        :param batch_size: batch size for dataloader
+        :param train: whether to load train or test data
+        :return dataloader: pytorch dataloader for mnist training dataset
+    """
+    def read_file_to_numpy(file_name):
+        mynumbers = []
+        with open(file_name) as f:
+            for line in f:
+                mynumbers.append(np.array([float(n) for n in line.strip().split()]))
+        return mynumbers
+
+    train_file,valid_file,test_file = [os.path.join(data_path, 'binarizedMNIST/binarized_mnist_' + ds + '.amat') for ds in ['train','valid','test']]
+
+    if train:
+        train_ar,valid_ar = [read_file_to_numpy(f) for f in [train_file,valid_file]]
+        train_ar = np.concatenate((train_ar, valid_ar), axis=0) #discard the valid set and put into the train one.
+        tensorData = torch.Tensor(train_ar)
+    else:
+        test_ar = read_file_to_numpy(test_file)
+        tensorData = torch.Tensor(test_ar)
+
+    tensorDatabaset = torch.utils.data.TensorDataset(tensorData)
+    dataloader = torch.utils.data.DataLoader(tensorDatabaset, batch_size=batch_size, shuffle=True)
+    return dataloader
+
+
+def fashion_mnist_dataloader(data_path: str, batch_size: int, train:bool=True):
+    """
+        Load cifar image data from specified, convert to grayscaled tensors, flatten, return dataloader
+        
+        :param data_path: full path to data directory
+        :param batch_size: batch size for dataloader
+        :param train: whether to load train or test data
+        :return dataloader: pytorch dataloader for mnist training dataset
+        """
+    # transforms to add to data
+    transform = torchvision.transforms.Compose([
+                                                torchvision.transforms.Grayscale(),
+                                                torchvision.transforms.ToTensor(),
+                                                lambda x: x>=0.5,
+                                                lambda x: x.float(),
+                                                TensorFlatten()
+                                                ])
+        
+    fashion_mnist_data = torchvision.datasets.FashionMNIST(data_path, transform=transform, train=train)
+    dataloader = torch.utils.data.DataLoader(fashion_mnist_data, batch_size=batch_size, shuffle=True)
+                                                
+    return dataloader
+
+def cifar_dataloader(data_path: str, batch_size: int, train:bool=True):
+    """
+        Load cifar image data from specified, convert to grayscaled tensors, flatten, return dataloader
+        
+        :param data_path: full path to data directory
+        :param batch_size: batch size for dataloader
+        :param train: whether to load train or test data
+        :return dataloader: pytorch dataloader for mnist training dataset
+        """
+    # transforms to add to data
+    transform = torchvision.transforms.Compose([
+                                                torchvision.transforms.Grayscale(),
+                                                torchvision.transforms.ToTensor(),
+                                                lambda x: x>=0.5,
+                                                lambda x: x.float(),
+                                                TensorFlatten()
+                                                ])
+        
+    mnist_data = torchvision.datasets.MNIST(data_path, transform=transform, train=train)
+    dataloader = torch.utils.data.DataLoader(mnist_data, batch_size=batch_size, shuffle=True)
+                                                
     return dataloader
