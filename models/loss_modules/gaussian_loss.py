80f830b11d763cc8a783fea6c0c0db17c43382a6
Maxence Draguet <maxdraguet@hotmail.com>
2020-03-15 20:54:22 +0000

    Cleaned

--- models/loss_modules/gaussian_loss.py
+++ models/loss_modules/gaussian_loss.py
@@ -32,41 +32,20 @@ class gaussianLoss(baseLoss):
         vae_reconstruction = vae_output['x_hat']
         vae_latent = vae_output['z']
         mean, log_var = vae_output['params']
-        #print("size vae_latent", vae_latent.size() )
-        #print("size mean", mean.size() )
-        #print("size x", x.size() )
-        #print("size vae_reconstruction", vae_reconstruction.size() )
-        #log(p(z)): N(0, I) thus logvariance is 0 and mean 0.
         logpz = -0.5 * (((vae_latent).pow(2)).sum(1))
-        #log(q(z)) : N(M, S)
         logqz = -0.5 * (log_var.sum(1) + ((vae_latent - mean).pow(2) / torch.exp(log_var)).sum(1))
-        #print("size logpz", logpz.size() )
-        #print("size logqz", logqz.size() )
+
         reconstruction_loss = F.binary_cross_entropy(vae_reconstruction, x, reduction='none').sum(-1) #this is the equivalent of log_bernouilli which is summed, not meaned.
-        
-        #print("size reconstruction_loss", reconstruction_loss.size() )
-        #print("Automatic: ", reconstruction_loss_2)
-        """
-        reconstruction_loss = -F.relu(vae_reconstruction) + torch.mul(x, vae_reconstruction) - torch.log(1. + torch.exp( -vae_reconstruction.abs() ))
-        while len(reconstruction_loss.size()) > 1:
-            reconstruction_loss = reconstruction_loss.sum(-1)
-            """
-        #print("Manual: ", reconstruction_loss.sum(-1))
+
         ELBO = -reconstruction_loss + logpz - logqz  # the last term should have a warmup constant
-        #print("logpz: ", logpz)
-        #print("step 1", ELBO)
+
         ELBO = ELBO.view(1, -1).transpose(0, 1)
-        #print("step 2", ELBO)
-        max_, _ = torch.max(ELBO, 1, keepdim=True)
-        #print("max_", max_)
+
+        #max_, _ = torch.max(ELBO, 1, keepdim=True)
         #ELBO = torch.log(torch.mean(torch.exp(ELBO - max_), 1)) + torch.squeeze(max_)
-        #print("step 3", ELBO)
+
         ELBO = torch.mean(ELBO)
-        #print("step 4", ELBO)
-        ELBO = -ELBO
-        return ELBO
 
-        # loss, rec, kl, bpd = calculate_loss(x_mean, data, z_mu, z_var, z0, zk, ldj, args, beta=beta)
-
-        # loss, rec, kl = binary_loss_function(x_mean, x, z_mu, z_var, z_0, z_k, ldj, beta=beta)
+        loss = -ELBO
+        return loss
 
