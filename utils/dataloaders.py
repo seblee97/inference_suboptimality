826c7a638b5683b9b7fe5efefb69c0a23fcbf874
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-03-16 14:32:36 +0000

    added dummy targets to binarised mnist dataloader to match api

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -29,12 +29,12 @@ def mnist_dataloader(data_path: str, batch_size: int, train:bool=True):
 
 def binarised_mnist_dataloader(data_path: str, batch_size: int, train:bool=True):
     """
-        Load binarised mnist image data from specified, convert to  tensors, flatten, return dataloader
-        
-        :param data_path: full path to data directory
-        :param batch_size: batch size for dataloader
-        :param train: whether to load train or test data
-        :return dataloader: pytorch dataloader for mnist training dataset
+    Load binarised mnist image data from specified, convert to  tensors, flatten, return dataloader
+    
+    :param data_path: full path to data directory
+    :param batch_size: batch size for dataloader
+    :param train: whether to load train or test data
+    :return dataloader: pytorch dataloader for mnist training dataset
     """
     def read_file_to_numpy(file_name):
         mynumbers = []
@@ -43,20 +43,22 @@ def binarised_mnist_dataloader(data_path: str, batch_size: int, train:bool=True)
                 mynumbers.append(np.array([float(n) for n in line.strip().split()]))
         return mynumbers
 
-    train_file,valid_file,test_file = [os.path.join(data_path, 'binarizedMNIST/binarized_mnist_' + ds + '.amat') for ds in ['train','valid','test']]
+    train_file, valid_file, test_file = [os.path.join(data_path, 'binarizedMNIST/binarized_mnist_' + ds + '.amat') for ds in ['train','valid','test']]
 
     if train:
-        train_ar,valid_ar = [read_file_to_numpy(f) for f in [train_file,valid_file]]
-        train_ar = np.concatenate((train_ar, valid_ar), axis=0) #discard the valid set and put into the train one.
+        train_ar, valid_ar = [read_file_to_numpy(f) for f in [train_file, valid_file]]
+        train_ar = np.concatenate((train_ar, valid_ar), axis=0) # merge train and val into larger train set
         tensorData = torch.Tensor(train_ar)
     else:
         test_ar = read_file_to_numpy(test_file)
         tensorData = torch.Tensor(test_ar)
 
-    tensorDatabaset = torch.utils.data.TensorDataset(tensorData)
-    dataloader = torch.utils.data.DataLoader(tensorDatabaset, batch_size=batch_size, shuffle=True)
-    return dataloader
+    targets = torch.Tensor([-1 for _ in range(len(tensorData))]).view(len(tensorData), -1) # these are dummy targets to match API
 
+    tensorDataset = torch.utils.data.TensorDataset(tensorData, targets)
+    dataloader = torch.utils.data.DataLoader(tensorDataset, batch_size=batch_size, shuffle=True)
+
+    return dataloader
 
 def fashion_mnist_dataloader(data_path: str, batch_size: int, train:bool=True):
     """
