0b02439822fc706baca4e689f503a94ffbc61ef1
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-03-17 20:58:06 +0000

    removed feedforward network - idea was to have construct layers be an abstract method of base flow and this is implemented now in the rnvp_flow child class

--- models/approximate_posteriors/flows/rnvp_flow.py
+++ models/approximate_posteriors/flows/rnvp_flow.py
@@ -1,24 +1,39 @@
 from .base_flow import BaseFlow
-from .nflow_feedforward import NFlowfeedForwardNetwork
-
 
 import torch.nn as nn
 import torch.nn.functional as F
+
+from typing import Dict
 
 
 class RNVP(BaseFlow):
     # TODO: doc
 
-    def __init__(self, config):
+    def __init__(self, config: Dict):
+
+        # half of the latent z size as input
+        self.input_dimension = config.get(["flow", "input_dim"])
+        self.latent_dimension = config.get(["flow", "latent_dim"])
+        self.hidden_dimensions = config.get(["flow", "flow_layers"])
+        self.activation_function = config.get(["flow", "nonlinearity"])
+        # The activation function is defined at mother class level as well as forward
 
         BaseFlow.__init__(self, config)
 
-        self.activation = nn.ELU()
-        self.softplus = nn.Softplus()
-        self.flow_network = NFlowfeedForwardNetwork(config=config)
-
     def _construct_layers(self):
-        pass
+
+        self.layers = nn.ModuleList([])
+
+        input_layer = self._initialise_weights(nn.Linear(self.input_dimension, self.hidden_dimensions[0]))
+        self.layers.append(input_layer)
+
+        for h in range(len(self.hidden_dimensions[:-1])):
+            hidden_layer = self._initialise_weights(nn.Linear(self.hidden_dimensions[h], self.hidden_dimensions[h + 1]))
+            self.layers.append(hidden_layer)
+
+        # final layer to latent dim
+        hidden_to_latent_layer = self._initialise_weights(nn.Linear(self.hidden_dimensions[-1], self.latent_dimension))
+        self.layers.append(hidden_to_latent_layer)
 
     def activation_derivative(self, x):
         """ Derivative of tanh """
