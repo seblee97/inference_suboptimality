4823d21a3103eaf802b688a54c8bf98c37102443
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-03-17 20:57:10 +0000

    added method for weight initialisation

--- models/approximate_posteriors/flows/base_flow.py
+++ models/approximate_posteriors/flows/base_flow.py
@@ -8,8 +8,26 @@ class BaseFlow(nn.Module, ABC):
 
         nn.Module.__init__(self)
 
+        self.initialisation = config.get(["flow", "weight_initialisation"])
         self._construct_layers()
 
     @abstractmethod
     def _construct_layers(self):
         raise NotImplementedError("Base class method")
+
+    def _initialise_weights(self, layer) -> None:
+        """
+        Weight initialisation method for given layer
+        """
+        if self.initialisation == "xavier_uniform":
+            nn.init.xavier_uniform_(layer.weight)
+            nn.init.constant_(layer.bias, 0)
+        elif self.initialisation == "xavier_normal":
+            nn.init.xavier_normal_(layer.weight)
+            nn.init.constant_(layer.bias, 0)
+        elif self.initialisation == "default":
+            pass
+        else:
+            raise ValueError("Initialisation {} not recognised".format(self.initialisation))
+
+        return layer
