d8d1d7474f887dd8f8d225552eef9a577083b1ea
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-03-25 12:10:18 +0000

    added warmup to rnvp

--- models/loss_modules/rnvp_loss.py
+++ models/loss_modules/rnvp_loss.py
@@ -17,7 +17,7 @@ class RNVPLoss(baseLoss):
     loss module :https://github.com/chrischute/real-nvp/blob/df51ad570baf681e77df4d2265c0f1eb1b5b646c/models/real_nvp/real_nvp_loss.py
     """
 
-    def compute_loss(self, x, vae_output):
+    def compute_loss(self, x, vae_output, warm_up):
 
         vae_reconstruction = vae_output['x_hat']
         vae_latent = vae_output['z']
@@ -34,7 +34,7 @@ class RNVPLoss(baseLoss):
         log_p_z = -0.5 * vae_latent.pow(2).sum(1)
         log_q_zx = -0.5 * (log_var.sum(1) + ((z0 - mean).pow(2) / torch.exp(log_var)).sum(1)) - log_det_jacobian
         # TODO: Add a warm-up constant to the last two terms.
-        log_p_x = log_p_xz + log_p_z - log_q_zx
+        log_p_x = log_p_xz + log_p_z - warm_up * log_q_zx
 
         # The ELBO is defined to be the mean of the logs in the batch.
         elbo = torch.mean(log_p_x)
