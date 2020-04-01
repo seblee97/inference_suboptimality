49377b303d466e660bcf27841f419d2513eca859
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-04-01 18:00:30 +0100

    get additional parameters implemented where flow module is initiated and the parameters are returned. Latent vector sample method implemented, which calls the forward (sample) method of flow module. Note mean and log var tensors are concatenated to match api of flow module

--- models/local_ammortisation_modules/rnvp_aux_flow_local_ammortisation.py
+++ models/local_ammortisation_modules/rnvp_aux_flow_local_ammortisation.py
@@ -13,5 +13,12 @@ class RNVPAuxLocalAmmortisation(BaseLocalAmmortisation):
         self._flow_module = None
         self._config = config
 
-    def sample_latent_vector(self):
-        pass
+    def get_additional_parameters(self):
+        self._flow_module = RNVPAux(config=self._config)
+        flow_parameters = self._flow_module.parameters()
+        return flow_parameters
+
+    def sample_latent_vector(self, params: torch.Tensor):
+        params_concat = torch.cat([params[0], params[1]], dim=1)
+        z, [mean, log_var, z0, log_det_jacobian, rv, rv_mean, rv_log_var] = self._flow_module.sample(params_concat)
+        return z, [mean, log_var, z0, log_det_jacobian, rv, rv_mean, rv_log_var]
