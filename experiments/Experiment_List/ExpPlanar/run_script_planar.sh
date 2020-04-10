496423517d1e64bf22b542423f950fc5a1f413e0
dianemagnin97 <dianemagnin@yahoo.com>
2020-04-10 23:44:59 +0100

    added a planar flow runner in experiments

---
+++ experiments/Experiment_List/ExpPlanar/run_script_planar.sh
@@ -0,0 +1,11 @@
+# Experiment Planar
+###############################################################################################
+#Convention:
+###############################################################################################
+# MNIST
+
+# Standard
+
+#Basic model for -MNIST trained on Planar flow
+python main.py --experiment_name exp_MNIST_planar --ap planar_flow
+
