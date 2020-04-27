2145252382242d892e14c30cca80ce26a10be942
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-04-27 17:11:46 +0100

    changed feedforward feedbackward filenames to fc_. Also changed class names within to be more accurate descriptions

--- models/networks/fc_decode.py
+++ models/networks/fc_decode.py
@@ -4,21 +4,21 @@ from typing import List, Dict
 
 import torch.nn as nn
 
-class feedForwardNetwork(baseNetwork):
+class FullyConnectedDecoderNetwork(baseNetwork):
 
     def __init__(self, config: Dict):
 
         self.input_dimension = config.get(["model", "input_dimension"])
         self.latent_dimension = config.get(["model", "latent_dimension"])
-        self.hidden_dimensions = config.get(["model", "encoder", "hidden_dimensions"])
-        self.factor = config.get(["model", "encoder", "output_dimension_factor"])
-        
+        self.hidden_dimensions = config.get(["model", "decoder", "hidden_dimensions"])
+
         baseNetwork.__init__(self, config=config)
-        
+
     def _construct_layers(self):
         
         self.layers = nn.ModuleList([])
-        self.layer_config = [self.input_dimension] + self.hidden_dimensions + [self.factor * self.latent_dimension]
+        
+        self.layer_config = [self.latent_dimension] + self.hidden_dimensions + [self.input_dimension]
         
         for h in range(len(self.layer_config)-1):
             self.layers.append(self._initialise_weights(nn.Linear(self.layer_config[h], self.layer_config[h + 1])))
