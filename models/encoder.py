2e767a38b26f92ef8f54d2f512c8d99dc894a100
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-04-27 17:21:17 +0100

    removed dead/hashed code

--- models/encoder.py
+++ models/encoder.py
@@ -20,15 +20,4 @@ class Encoder(nn.Module, ABC):
         approximate_posterior_parameters = self.network(x)
         latent_vector, params = self.approximate_posterior.sample(approximate_posterior_parameters)
 
-        # XXX: should we also return a field akin to 'loss_metrics' in the dictionary
-        # for downstream use in calculating the losses e.g. logdetj for the flow modules
-
         return {'z': latent_vector, 'params': params}
-
-# # compute probability of sample under q
-#         log_pqz = log_normal(latent_vector, mu, var)
-
-#         import pdb; pdb.set_trace()
-
-#         # compute prior probability p(z)
-#         logpz = log_normal(latent_vector, torch.zeros(latent_vector.shape), torch.ones(latent_vector.shape[0]))
