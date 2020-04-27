03241894921c1d9d7a3d53ed1039a9e7a5b5bb92
Mandrenkov <M.Andrenkov@gmail.com>
2020-04-27 16:24:34 +0100

    Added standard deviation parameter to normal weight initialisation.

--- models/approximate_posteriors/base_norm_flow.py
+++ models/approximate_posteriors/base_norm_flow.py
@@ -11,6 +11,9 @@ class BaseFlow(nn.Module, ABC):
         nn.Module.__init__(self)
 
         self.initialisation = config.get(["flow", "weight_initialisation"])
+        if self.initialisation == "normal":
+            self.initialisation_std = config.get(["flow", "weight_initialisation_std"])
+
         self._construct_layers()
 
         self.activation_function = config.get(["flow", "nonlinearity"])
@@ -35,9 +38,9 @@ class BaseFlow(nn.Module, ABC):
         elif self.initialisation == "xavier_normal":
             nn.init.xavier_normal_(layer.weight)
             nn.init.constant_(layer.bias, 0)
-        elif self.initialisation == "planar_normal":
-            nn.init.normal_(layer.weight, std=1E-4)
-            nn.init.normal_(layer.bias, std=1E-4)
+        elif self.initialisation == "normal":
+            nn.init.normal_(layer.weight, std=self.initialisation_std)
+            nn.init.normal_(layer.bias, std=self.initialisation_std)
         elif self.initialisation == "default":
             pass
         else:
