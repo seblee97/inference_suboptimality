2737683e685849b3a4fa112976d0cee988d13bc5
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-04-28 14:36:03 +0100

    fixed comment on elbo description to include auxvariable reverse term

--- models/loss_modules/rnvp_aux_loss.py
+++ models/loss_modules/rnvp_aux_loss.py
@@ -5,7 +5,7 @@ from typing import Dict
 import torch.nn.functional as F
 import torch
 import numpy as np
-from typing import Dict
+from typing import Dict, Tuple
 
 class RNVPAuxLoss(_BaseLoss):
 
@@ -13,11 +13,7 @@ class RNVPAuxLoss(_BaseLoss):
 
         _BaseLoss.__init__(self)
 
-    """
-    loss module :https://github.com/chrischute/real-nvp/blob/df51ad570baf681e77df4d2265c0f1eb1b5b646c/models/real_nvp/real_nvp_loss.py
-    """
-
-    def compute_loss(self, x, vae_output, warm_up=1) -> (torch.Tensor, Dict, torch.Tensor):
+    def compute_loss(self, x, vae_output, warm_up=1) -> Tuple[torch.Tensor, Dict, torch.Tensor]:
 
         vae_reconstruction = vae_output['x_hat']
         vae_latent = vae_output['z']
@@ -25,11 +21,12 @@ class RNVPAuxLoss(_BaseLoss):
 
         # Calculate the logs in the ELBO with ONE sample from the expectation.
         # Flow adds extra term - the sum of the logs of the determinants of the transformation Jacobians.
-        #   ELBO = E[log(p(x,z) / q(z|x))]
-        #        = E[log(p(x|z) * p(z) / q(z|x))]
-        #        = E[log p(x|z) + log p(z) - log q(z|x)]
+        # Aux adds reverse log prob term.
+        #   ELBO = E[log(p(x,z) * r(v|x,z) / (q(z,v|x) * sum(log_det_jacobian)))]
+        #        = E[log(p(x|z) * p(z) * r(v|x,z) / (q(z,v|x)) * sum(det_jacobian))]
+        #        = E[log p(x|z) + log p(z) + log r(v|x,z) - log q(z,v|x) - sum(log_det_jacobian)]
         #          explicit flow expression
-        #        = E[log p(x|z) + log p(z) - log q_0(z_0|x) + sum(log_det_jacobian)]
+        #        = E[log p(x|z) + log p(z) + log r(v|x,z) - log q_0(z_0, v_0|x) + sum(log_det_jacobian)]
         log_p_xz = -F.binary_cross_entropy_with_logits(vae_reconstruction, x, reduction='none').view(x.shape[0], -1).sum(-1)
         log_p_z = -0.5 * vae_latent.pow(2).sum(1)
         log_q_zx = -0.5 * (log_var.sum(1) + ((z0 - mean).pow(2) / torch.exp(log_var)).sum(1)) - log_det_jacobian
@@ -37,7 +34,6 @@ class RNVPAuxLoss(_BaseLoss):
         # reverse model log
         log_r_vxz = -0.5 * (rv_log_var.sum(1) + ((rv - rv_mean).pow(2) / torch.exp(rv_log_var)).sum(1))
 
-        # TODO: Add a warm-up constant to the last two terms.
         log_p_x = log_p_xz + log_p_z - warm_up * (log_q_zx - log_r_vxz)
 
         # The ELBO is defined to be the mean of the logs in the batch.
