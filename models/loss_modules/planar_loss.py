ddbfc3cf516939270ce3cdc5751ae26a7a2c5a82
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-04-28 14:30:00 +0100

    removed dud comment and todo, changed tuple typing syntax

--- models/loss_modules/planar_loss.py
+++ models/loss_modules/planar_loss.py
@@ -5,7 +5,7 @@ from typing import Dict
 import torch.nn.functional as F
 import torch
 import numpy as np
-from typing import Dict
+from typing import Dict, Tuple
 
 class PlanarLoss(_BaseLoss):
 
@@ -13,11 +13,7 @@ class PlanarLoss(_BaseLoss):
 
         _BaseLoss.__init__(self)
 
-    """
-    loss module :https://github.com/chrischute/real-nvp/blob/df51ad570baf681e77df4d2265c0f1eb1b5b646c/models/real_nvp/real_nvp_loss.py
-    """
-
-    def compute_loss(self, x, vae_output, warm_up=1) -> (torch.Tensor, Dict, torch.Tensor):
+    def compute_loss(self, x, vae_output, warm_up=1) -> Tuple[torch.Tensor, Dict, torch.Tensor]:
 
         vae_reconstruction = vae_output['x_hat']
         vae_latent = vae_output['z']
@@ -34,7 +30,7 @@ class PlanarLoss(_BaseLoss):
         log_p_xz = -F.binary_cross_entropy_with_logits(vae_reconstruction, x, reduction='none').view(x.shape[0], -1).sum(-1)
         log_p_z = -0.5 * vae_latent.pow(2).sum(1)
         log_q_zx = -0.5 * (log_var.sum(1) + ((z0 - mean).pow(2) / torch.exp(log_var)).sum(1)) - log_det_jacobian
-        # TODO: Add a warm-up constant to the last two terms.
+
         log_p_x = log_p_xz + log_p_z - warm_up * log_q_zx
 
         # The ELBO is defined to be the mean of the logs in the batch.
